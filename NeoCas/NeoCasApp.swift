1735e7ceb858274d7cf1ab1e1c8ae353831f317d
keshew <artyom.korotckov2017@yandex.ru>
2025-10-12 17:03:53 +0500

    Initial Commit

---
+++ NeoCas/NeoCasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoCasApp.swift
+//  NeoCas
+//
+//  Created by Артём Коротков on 12.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct NeoCasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
